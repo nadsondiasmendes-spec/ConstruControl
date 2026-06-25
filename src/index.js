3d943b556b3fe549c4e42b1090790d3cd0bfeee2
Nadson Dias Mendes <nadsondiasmendes@gmail.com>
2026-06-25 17:38:35 -0300

    fix: restauracao do tailwind v3.4 e correcoes de layout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Isso importa seu App.js
+import './index.css'; // <--- ESTA LINHA É OBRIGATÓRIA!
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
